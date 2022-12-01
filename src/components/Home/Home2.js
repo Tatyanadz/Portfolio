0df88fe51f7990daa71b88865edb84490014d18f
TatyanaDz <48102770+Tatyanadz@users.noreply.github.com>
2022-12-01 16:14:47 -0500

    working on home page

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,27 +19,16 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+            I like things that make sense. Coding makes sense. I mean, most of the time it does...🤷‍♂️
               <br />
-              <br />I am fluent in classics like
+              <br />My motto is simple: 
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”</b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Whenever possible, I apply my passion for developing products
+              with <b className="purple"> Node.js</b> and
               <i>
                 <b className="purple">
                   {" "}
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Software Developer",
+          "Code Enthusiast",
+          
         ],
         autoStart: true,
         loop: true,
